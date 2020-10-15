2c4940516464e1d4f518e9fe23df2b91fd3c74de
joonasrom <jpr97@luukku.com>
2020-10-15 15:31:13 +0300

    added postgres

--- bookstore/src/test/java/com/example/bookstore/BookstoreApplicationTests.java
+++ bookstore/src/test/java/com/example/bookstore/BookstoreApplicationTests.java
@@ -3,15 +3,15 @@ package com.example.bookstore;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.example.bookstore.model.UserRepository;
 import com.example.bookstore.web.BookController;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class BookstoreApplicationTests {
 	
