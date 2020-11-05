58c1221b8e8d3565865e23623d16902fe83feec2
joonasrom <jpr97@luukku.com>
2020-11-05 15:20:04 +0200

    added clearing method for startup

--- bookstore/src/main/java/com/example/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -29,20 +29,22 @@ public class BookstoreApplication {
 			Category c3 = new Category("Novel");
 			Category c4 = new Category("Humor");
 
+			crepository.deleteAll();
 			crepository.save(c1);
 			crepository.save(c2);
 			crepository.save(c3);
 			crepository.save(c4);
 
+			
 			Book b1 = new Book("Harry Potter and the Philosopher's stone", "J.K.Rowling", 1997, "564-2-9-5624-2", 26.95,
 					crepository.findByName("Fantasy").get(0));
 			Book b2 = new Book("Frankenstein", "Mary Shelley", 1823, "978-0-30-5151-1", 132.95,
 					crepository.findByName("Horror").get(0));
-			
+			repository.deleteAll();
 			repository.save(b1);
 			repository.save(b2);
 			
-			
+			urepository.deleteAll();
 			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", "user@bookstore.fi");
 			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN", "admin@bookstore.fi");
 			urepository.save(user1);
